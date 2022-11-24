37435be084437486c9f693a0ae0222f4f531420f
Seetha Lakshmi.G.M <seethalakshmi1607@gmail.com>
2022-11-24 22:58:44 +0800

    initial commit

---
+++ src/main/java/com/example/BejC2S1RestApiMongodbMc1Application.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BejC2S1RestApiMongodbMc1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BejC2S1RestApiMongodbMc1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/BejC2S1RestApiMongodbMc1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BejC2S1RestApiMongodbMc1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
