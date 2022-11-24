94bf4c5a1159f52f6c9e0ef0c8789468e5b684b7
Seetha Lakshmi.G.M <seethalakshmi1607@gmail.com>
2022-11-25 02:28:21 +0800

    created two custom exception class to be implemented in the functions

---
+++ src/main/java/com/example/exception/TrackNotExists.java
@@ -0,0 +1,15 @@
+/*
+ *Author Name : Seetha Lakshmi.G.M
+ *Date : 25-11-2022
+ *Created With : IntelliJ IDEA Community Edition
+ */
+
+
+package com.example.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(code = HttpStatus.CONFLICT, reason = "Track with Specific code already Exists. Try some other code!")
+public class TrackNotExists extends Exception {
+}
