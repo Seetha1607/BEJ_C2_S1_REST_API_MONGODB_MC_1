606e797492ef598f8ca7ddfc557e10ba95cc1a32
Seetha Lakshmi.G.M <seethalakshmi1607@gmail.com>
2022-11-24 23:14:31 +0800

    created Constant CRUD abstract methods in ITrackService.java and implemented in TrackServiceImpl.java to handle it

--- src/main/java/com/example/service/ITrackService.java
+++ src/main/java/com/example/service/ITrackService.java
@@ -1,4 +1,13 @@
 package com.example.service;
 
+import com.example.domain.Track;
+
+import java.util.List;
+
 public interface ITrackService {
+    public Track saveTrackDetails(Track track);
+
+    public List<Track> fetchAllTrack();
+
+    public boolean deleteTrack(int trackId);
 }
--- src/main/java/com/example/service/TrackServiceImpl.java
+++ src/main/java/com/example/service/TrackServiceImpl.java
@@ -7,5 +7,25 @@
 
 package com.example.service;
 
-public class TrackServiceImpl {
+import com.example.domain.Track;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class TrackServiceImpl implements ITrackService {
+    @Override
+    public Track saveTrackDetails(Track track) {
+        return null;
+    }
+
+    @Override
+    public List<Track> fetchAllTrack() {
+        return null;
+    }
+
+    @Override
+    public boolean deleteTrack(int trackId) {
+        return false;
+    }
 }
