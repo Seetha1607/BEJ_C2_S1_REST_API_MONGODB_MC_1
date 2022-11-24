7c8acbf31a0728b5dc4cc1f7e89d310946e6cca7
Seetha Lakshmi.G.M <seethalakshmi1607@gmail.com>
2022-11-24 23:16:49 +0800

    autowired the TrackRepository using constructor method in TrackServiceImpl.java

--- src/main/java/com/example/service/TrackServiceImpl.java
+++ src/main/java/com/example/service/TrackServiceImpl.java
@@ -8,12 +8,22 @@
 package com.example.service;
 
 import com.example.domain.Track;
+import com.example.repository.TrackRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class TrackServiceImpl implements ITrackService {
+
+    private TrackRepository trackRepository;
+
+    @Autowired
+    public TrackServiceImpl(TrackRepository trackRepository) {
+        this.trackRepository = trackRepository;
+    }
+
     @Override
     public Track saveTrackDetails(Track track) {
         return null;
