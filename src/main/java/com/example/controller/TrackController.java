de5158f9df52795251ac9212bfb99df44096d0d5
Seetha Lakshmi.G.M <seethalakshmi1607@gmail.com>
2022-11-24 23:26:04 +0800

    used the request method to connect with database and get perform the specific fucntions

--- src/main/java/com/example/controller/TrackController.java
+++ src/main/java/com/example/controller/TrackController.java
@@ -7,10 +7,12 @@
 
 package com.example.controller;
 
+import com.example.domain.Track;
 import com.example.service.ITrackService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1")
@@ -20,5 +22,20 @@ public class TrackController {
     @Autowired
     public TrackController(ITrackService iTrackService) {
         this.iTrackService = iTrackService;
+    }
+
+    @PostMapping("/insertTrackDetails")
+    public ResponseEntity<?> saveTrackDetails(@RequestBody Track track) {
+        return new ResponseEntity<>(iTrackService.saveTrackDetails(track), HttpStatus.CREATED);
+    }
+
+    @GetMapping("/fetchTrackDetails")
+    public ResponseEntity<?> fetchTrackDetails() {
+        return new ResponseEntity<>(iTrackService.fetchAllTrack(), HttpStatus.ACCEPTED);
+    }
+
+    @DeleteMapping("/deleteTrack/{trackId}")
+    public ResponseEntity<?> deleteTrack(@PathVariable int trackId) {
+        return new ResponseEntity<>(iTrackService.deleteTrack(trackId), HttpStatus.OK);
     }
 }
