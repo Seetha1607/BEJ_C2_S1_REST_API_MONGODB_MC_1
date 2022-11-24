6ce5bcc37398fe14ac9ebb4826bff5e5fd7dfaca
Seetha Lakshmi.G.M <seethalakshmi1607@gmail.com>
2022-11-24 23:20:14 +0800

    created the TrackController.java

---
+++ src/main/java/com/example/controller/TrackController.java
@@ -0,0 +1,16 @@
+/*
+ *Author Name : Seetha Lakshmi.G.M
+ *Date : 24-11-2022
+ *Created With : IntelliJ IDEA Community Edition
+ */
+
+
+package com.example.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1")
+public class TrackController {
+}
