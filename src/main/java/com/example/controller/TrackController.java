06803f512d27a39e8a8ab7c8cffa7b7c48469699
Seetha Lakshmi.G.M <seethalakshmi1607@gmail.com>
2022-11-24 23:21:37 +0800

    autowired the ITrackService using constructor method in TrackController.java

--- src/main/java/com/example/controller/TrackController.java
+++ src/main/java/com/example/controller/TrackController.java
@@ -7,10 +7,18 @@
 
 package com.example.controller;
 
+import com.example.service.ITrackService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1")
 public class TrackController {
+    private ITrackService iTrackService;
+
+    @Autowired
+    public TrackController(ITrackService iTrackService) {
+        this.iTrackService = iTrackService;
+    }
 }
