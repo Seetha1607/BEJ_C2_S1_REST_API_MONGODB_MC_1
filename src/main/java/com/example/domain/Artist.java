d6db036cdeeefea41db6979a2006955bce1bbee0
Seetha Lakshmi.G.M <seethalakshmi1607@gmail.com>
2022-11-24 23:09:11 +0800

    added no args and parameterized constructor,toString(), getter() and setter() methods in Track and Artist class

--- src/main/java/com/example/domain/Artist.java
+++ src/main/java/com/example/domain/Artist.java
@@ -7,8 +7,41 @@
 
 package com.example.domain;
 
+import java.util.StringJoiner;
+
 public class Artist {
     private int artistId;
     private String artistName;
 
+    public Artist() {
+    }
+
+    public Artist(int artistId, String artistName) {
+        this.artistId = artistId;
+        this.artistName = artistName;
+    }
+
+    public int getArtistId() {
+        return artistId;
+    }
+
+    public void setArtistId(int artistId) {
+        this.artistId = artistId;
+    }
+
+    public String getArtistName() {
+        return artistName;
+    }
+
+    public void setArtistName(String artistName) {
+        this.artistName = artistName;
+    }
+
+    @Override
+    public String toString() {
+        return new StringJoiner(", ", Artist.class.getSimpleName() + "[", "]")
+                .add("artistId=" + artistId)
+                .add("artistName='" + artistName + "'")
+                .toString();
+    }
 }
--- src/main/java/com/example/domain/Track.java
+++ src/main/java/com/example/domain/Track.java
@@ -10,6 +10,8 @@ package com.example.domain;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import java.util.StringJoiner;
+
 @Document
 public class Track {
     @Id
@@ -18,5 +20,55 @@ public class Track {
     private float trackRating;
     private Track trackArtist;
 
+    public Track() {
+    }
 
+    public Track(int trackId, String trackName, float trackRating, Track trackArtist) {
+        this.trackId = trackId;
+        this.trackName = trackName;
+        this.trackRating = trackRating;
+        this.trackArtist = trackArtist;
+    }
+
+    public int getTrackId() {
+        return trackId;
+    }
+
+    public void setTrackId(int trackId) {
+        this.trackId = trackId;
+    }
+
+    public String getTrackName() {
+        return trackName;
+    }
+
+    public void setTrackName(String trackName) {
+        this.trackName = trackName;
+    }
+
+    public float getTrackRating() {
+        return trackRating;
+    }
+
+    public void setTrackRating(float trackRating) {
+        this.trackRating = trackRating;
+    }
+
+    public Track getTrackArtist() {
+        return trackArtist;
+    }
+
+    public void setTrackArtist(Track trackArtist) {
+        this.trackArtist = trackArtist;
+    }
+
+    @Override
+    public String toString() {
+        return new StringJoiner(", ", Track.class.getSimpleName() + "[", "]")
+                .add("trackId=" + trackId)
+                .add("trackName='" + trackName + "'")
+                .add("trackRating=" + trackRating)
+                .add("trackArtist=" + trackArtist)
+                .toString();
+    }
 }
